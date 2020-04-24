0d0a6ab250aeee2a2c87b9997422e1e85a9ef63b
awaludin12269 <55430717+awaludin12269@users.noreply.github.com>
2020-04-24 16:49:15 +0700

    lupa_pasword mendaftar login

---
+++ Papikos/app/src/main/java/com/ludfyrahman/papikos/ubah_password.java
@@ -0,0 +1,14 @@
+package com.ludfyrahman.papikos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ubah_password extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_password);
+    }
+}
