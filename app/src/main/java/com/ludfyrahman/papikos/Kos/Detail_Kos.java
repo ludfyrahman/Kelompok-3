3106890dad6094ad3137915cde6778b55902b5d6
ludfyrahman <ludfyr@gmail.com>
2020-05-09 15:22:55 +0700

    detail kos

---
+++ app/src/main/java/com/ludfyrahman/papikos/Kos/Detail_Kos.java
@@ -0,0 +1,16 @@
+package com.ludfyrahman.papikos.Kos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ludfyrahman.papikos.R;
+
+public class Detail_Kos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_kos);
+    }
+}
