899182cf92383f6166a5a4d4e308800322517157
e41180658muhammadjamalludin <mochjamalludin@gmail.com>
2020-05-15 16:16:00 +0700

    Layout transaksi Berhasil

---
+++ Papikos/app/src/main/java/com/example/papikos/transaksiBerhasilActivity.java
@@ -0,0 +1,14 @@
+package com.example.mypapikos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class transaksiBerhasilActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transaksi_berhasil);
+    }
+}
