858cc2e6b9a5bda95d77c0555dfba2bf2fdefcc8
ludfyrahman <ludfyr@gmail.com>
2020-05-17 02:04:03 +0700

    notifikasi config

--- app/src/main/java/com/ludfyrahman/papikos/Config/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/ludfyrahman/papikos/Config/MyFirebaseInstanceIdService.java
@@ -1,19 +1,20 @@
 package com.ludfyrahman.papikos.Config;
 import android.util.Log;
 
-//import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceId;
+
 
 public class MyFirebaseInstanceIdService {
     private static final String REG_TOKEN = "REG_TOKEN";
 
     public void onTokenRefresh(String token) {
-//        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
-//        Log.d(REG_TOKEN, "Refreshed token Baru: " + token);
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+        Log.d(REG_TOKEN, "Refreshed token Baru: " + token);
 
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // Instance ID token to your app server.
-//        sendRegistrationToServer(token);
+//         If you want to send messages to this application instance or
+//         manage this apps subscriptions on the server side, send the
+//         Instance ID token to your app server.
+        sendRegistrationToServer(token);
     }
     private void sendRegistrationToServer(String token) {
     }
